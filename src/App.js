465b1852df73561419f0c1409c30603c7c4419bc
VickersCode <tyler31vickers@gmail.com>
2023-07-07 18:28:20 -0700

    Add handlePlay func

--- src/App.js
+++ src/App.js
@@ -79,6 +79,10 @@ export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquares = history[history.length - 1];
 
+  function handlePlay(nextSquares) {
+    
+  }
+
   return (
     <div className="game">
       <div className="game-board">
